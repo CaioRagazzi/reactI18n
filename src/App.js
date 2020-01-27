a11b7bf9f1db41c2f930587e5105c609da312e6b
Caio <ca.ragazzi@gmail.com>
2020-01-26 22:31:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+
+  const { t, i18n } = useTranslation();
+
+  const handleClick = (language) => {
+    i18n.changeLanguage(language);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="App">
+        <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
+          <button onClick={() => handleClick('en')}>English</button>
+          <button onClick={() => handleClick('pt')}>Portuguese</button>
+        </nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          {t('Text.1')}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </header>
+      </div>
   );
 }
 
